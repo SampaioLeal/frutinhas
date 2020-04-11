758f5a2a1ad4e143e9bd143aa814f581cc5f49ce
Sampaio Leal <samp14hd@gmail.com>
2020-04-11 07:02:10 -0300

    Updates

---
+++ precache-manifest.0b7849d5e9c3a8dbb55e5959f57f6dd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c2bccf616eeb6028f23a6db71fa603",
+    "url": "/frutinhas/index.html"
+  },
+  {
+    "revision": "288b8f585a782c20d994",
+    "url": "/frutinhas/static/css/main.0b2091ff.chunk.css"
+  },
+  {
+    "revision": "33f69031bb6b1d15b16f",
+    "url": "/frutinhas/static/js/2.856568ab.chunk.js"
+  },
+  {
+    "revision": "d4b05998a178e6fb79e4761de5ed64fc",
+    "url": "/frutinhas/static/js/2.856568ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "288b8f585a782c20d994",
+    "url": "/frutinhas/static/js/main.ccb42eff.chunk.js"
+  },
+  {
+    "revision": "d107621e143506ba8775",
+    "url": "/frutinhas/static/js/runtime-main.b352ac6c.js"
+  }
+]);
