0d34eb5d207a30ad5f0cd1f0d94b9e6d1a671383
Sampaio Leal <samp14hd@gmail.com>
2020-04-11 07:04:32 -0300

    https :(

--- src/utils/SocketListener.js
+++ src/utils/SocketListener.js
@@ -3,9 +3,9 @@ import { observer } from "mobx-react";
 import io from "socket.io-client";
 
 // Generate a socket.io instance
-const socket = io("http://192.81.217.9/frutinhas", {
+const socket = io("https://192.81.217.9/frutinhas", {
 	forceNew: true,
-	autoConnect: true
+	autoConnect: true,
 });
 
 function SocketListener({ state }) {
@@ -18,8 +18,8 @@ function SocketListener({ state }) {
 
 	// Update state for players and fruits
 	useEffect(() => {
-		socket.on("update", update => {
-			update.players.map(player => {
+		socket.on("update", (update) => {
+			update.players.map((player) => {
 				if (player.id === socket.id) player.current = true;
 			});
 			state.updatePlayers(update.players);
