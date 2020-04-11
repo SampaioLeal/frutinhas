7b94be1973a169e73e6863f115367e598e0ed0db
Sampaio Leal <samp14hd@gmail.com>
2020-04-11 07:47:36 -0300

    :)

--- src/utils/SocketListener.js
+++ src/utils/SocketListener.js
@@ -3,7 +3,7 @@ import { observer } from "mobx-react";
 import io from "socket.io-client";
 
 // Generate a socket.io instance
-const socket = io("https://192.81.217.9/frutinhas", {
+const socket = io("http://192.81.217.9/frutinhas/", {
 	forceNew: true,
 	autoConnect: true,
 });
